87f6e5950b2ac936a233da25ac733305529fdc90
Your Name <you@example.com>
2023-05-03 11:59:23 +0530

    updates

--- src/Utility/Utility.js
+++ src/Utility/Utility.js
@@ -1,32 +1,8 @@
+import { useState, useEffect } from "react";
+
 const Api = () => {
-  ApiUtil(`
-  query {
-    products(
-      filter: { sku: { eq: "24-MB03" } }
-      pageSize: 20
-      currentPage: 1
-      sort: {}
-    ) {
-      items {
-        name
-        image {
-          url
-        }
-        price_range {
-          maximum_price {
-            final_price {
-              value
-            }
-          }
-        }
-        description {
-          html
-        }
-        
-      }
-    }
-  }
-`);
+  const [data, setData] = useState(null);
+
   async function ApiUtil(query) {
     const response = await fetch("/graphql", {
       method: "POST",
@@ -37,17 +13,57 @@ const Api = () => {
         query,
       }),
     });
-    const data = await response.json();
+    const { data } = await response.json();
+    setData(data);
     console.log(data);
-    return data;
   }
 
-  <div>
-    {Object.keys().map((item, index) => (
-      <div key={index}>
-        <div></div>
-      </div>
-    ))}{" "}
-  </div>;
+  useEffect(() => {
+    ApiUtil(`
+      query {
+        products(
+          filter: { sku: { eq: "24-MB03" } }
+          pageSize: 20
+          currentPage: 1
+          sort: {}
+        ) {
+          items {
+            name
+            image {
+              url
+            }
+            price_range {
+              maximum_price {
+                final_price {
+                  value
+                }
+              }
+            }
+            description {
+              html
+            }
+          }
+        }
+      }
+    `);
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        data.products.items.map((item, index) => (
+          <div key={index}>
+            <div>{item.name}</div>
+            <img src={item.image.url} alt={item.name} />
+            <div>{item.price_range.maximum_price.final_price.value}</div>
+            <div dangerouslySetInnerHTML={{ __html: item.description.html }} />
+          </div>
+        ))
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
+
 export default Api;
