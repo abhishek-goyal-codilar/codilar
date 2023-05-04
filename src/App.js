62d77ef7ce7d93b9c56918ff2681b58f505053b2
Your Name <you@example.com>
2023-05-04 22:31:33 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Content from "./Content/Content";
 import OtherProduct from "./otherProducts/otherPoducts";
 // import Utility from "./Utility/Utility";
 // import Api from "./Utility/Utility";
+import CopyRight from "./Copyright/Copyright";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       {/* <Api /> */}
       <OtherProduct />
       <Footer />
+      <CopyRight />
     </div>
   );
 }
---
+++ src/Copyright/Copyright.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./copyright.css";
+
+const CopyRight = () => {
+  return (
+    <small className="copyright">
+      <span>Copyright © 2013-present Magento, Inc. All rights reserved.</span>
+    </small>
+  );
+};
+
+export default CopyRight;
