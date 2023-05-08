068d59a33457c10092dc5716060e4a4b6b45224f
Your Name <you@example.com>
2023-05-08 13:19:00 +0530

    updates

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -4,20 +4,17 @@ import { useState, useEffect } from "react";
 
 const Content = () => {
   const [data, setData] = useState(null);
-  const [selectedButton, setSelectedButton] = useState(null);
-  const [displayText, setDisplayText] = useState("");
-  // const [disabled, setDisabled] = useState(false);
+  const [activeButton, setActiveButton] = useState(1);
 
-  const handleButtonClick = (buttonId, text) => {
-    setSelectedButton(buttonId);
-    setDisplayText(text);
+  const handleButtonClick = (buttonId) => {
+    setActiveButton(buttonId);
   };
 
   useEffect(() => {
     ApiUtil(`
     query {
       products(
-        filter: {            
+        filter: {
           sku: { eq: "24-MB03" } }
         pageSize: 20
         currentPage: 1
@@ -52,7 +49,7 @@ const Content = () => {
         }
       }
     }
-    
+
     `);
     async function ApiUtil(query) {
       const response = await fetch("/graphql", {
@@ -122,119 +119,112 @@ const Content = () => {
             <div>
               <button
                 className="product-info-more-button"
-                onClick={() =>
-                  handleButtonClick(
-                    1,
-                    <div className="product-info-description">
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html: item.description.html,
-                        }}
-                      />
-                    </div>
-                  )
-                }
+                onClick={() => handleButtonClick(1)}
               >
                 Details
               </button>
+
               <button
                 className="product-info-more-button"
-                onClick={() =>
-                  handleButtonClick(
-                    2,
-                    <div className="product-info-description">
-                      <table>
-                        <tbody>
-                          <tr>
-                            <th className="column-head" scope="row">
-                              Activity
-                            </th>
-                            <td className="column-data" data-th="Activity">
-                              Gym, Hiking, Overnight, School, Trail, Travel,
-                              Urban
-                            </td>
-                          </tr>
-                          <tr>
-                            <th className="column-head" scope="row">
-                              Style
-                            </th>
-                            <td className="column-data" data-th="Activity">
-                              Backpack
-                            </td>
-                          </tr>
-                          <tr>
-                            <th className="column-head" scope="row">
-                              Material
-                            </th>
-                            <td className="column-data" data-th="Activity">
-                              Nylon, Polyester
-                            </td>
-                          </tr>
-                          <tr>
-                            <th className="column-head" scope="row">
-                              Strap/Handle
-                            </th>
-                            <td className="column-data" data-th="Activity">
-                              Adjustable, Double, Padded
-                            </td>
-                          </tr>
-                          <tr>
-                            <th className="column-head" scope="row">
-                              Features
-                            </th>
-                            <td className="column-data" data-th="Activity">
-                              Audio Pocket, Waterproof, Lightweight, Reflective,
-                              Laptop Sleeve
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  )
-                }
+                onClick={() => handleButtonClick(2)}
               >
                 More Information
               </button>
+
               <button
                 className="product-info-more-button"
-                onClick={() =>
-                  handleButtonClick(
-                    3,
-                    <div className="product-info-description">
-                      <p className="list-item-header">Customer Review</p>
-                      {item.reviews.items.map((review, index) => {
-                        return (
-                          <ol key={index} className="order-list-info">
-                            <li className="list-item-info">
-                              <p className="review-header">{review.summary}</p>
-                              <div className="c">
-                                <div className="a">
-                                  <p className="review-rating">Rating </p>
-                                  <i class="fa-solid fa-star"></i>
-                                </div>
-                                {/* <p>{review.ratings_breakdown.name}</p> */}
-                                <div className="b">
-                                  <p className="review-description">
-                                    {review.text}
-                                  </p>
-                                  <p>Review by {review.nickname}</p>
-                                  <p>{review.created_at}</p>
-                                </div>
-                              </div>
-                            </li>
-                          </ol>
-                        );
-                      })}
-                    </div>
-                  )
-                }
+                onClick={() => handleButtonClick(3)}
               >
                 Reviews
               </button>
+
               <div>
-                {/* {selectedButton && (
-                )} */}
-                <p className="displayPText">{displayText}</p>
+                {activeButton === 1 && (
+                  <div className="product-info-description">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: item.description.html,
+                      }}
+                    />
+                  </div>
+                )}
+                {activeButton === 2 && (
+                  <div className="product-info-description">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <th className="column-head" scope="row">
+                            Activity
+                          </th>
+                          <td className="column-data" data-th="Activity">
+                            Gym, Hiking, Overnight, School, Trail, Travel, Urban
+                          </td>
+                        </tr>
+                        <tr>
+                          <th className="column-head" scope="row">
+                            Style
+                          </th>
+                          <td className="column-data" data-th="Activity">
+                            Backpack
+                          </td>
+                        </tr>
+                        <tr>
+                          <th className="column-head" scope="row">
+                            Material
+                          </th>
+                          <td className="column-data" data-th="Activity">
+                            Nylon, Polyester
+                          </td>
+                        </tr>
+                        <tr>
+                          <th className="column-head" scope="row">
+                            Strap/Handle
+                          </th>
+                          <td className="column-data" data-th="Activity">
+                            Adjustable, Double, Padded
+                          </td>
+                        </tr>
+                        <tr>
+                          <th className="column-head" scope="row">
+                            Features
+                          </th>
+                          <td className="column-data" data-th="Activity">
+                            Audio Pocket, Waterproof, Lightweight, Reflective,
+                            Laptop Sleeve
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                )}
+                {activeButton === 3 && (
+                  <div className="product-info-description">
+                    <p className="list-item-header">Customer Review</p>
+                    {item.reviews.items.map((review, index) => {
+                      return (
+                        <ol key={index} className="order-list-info">
+                          <li className="list-item-info">
+                            <p className="review-header">{review.summary}</p>
+                            <div className="c">
+                              <div className="a">
+                                <p className="review-rating">Rating </p>
+                                <i class="fa-solid fa-star"></i>
+                              </div>
+                              {/* <p>{review.ratings_breakdown.name}</p> */}
+                              <div className="b">
+                                <p className="review-description">
+                                  {review.text}
+                                </p>
+                                <p>Review by {review.nickname}</p>
+                                <p>{review.created_at}</p>
+                              </div>
+                            </div>
+                          </li>
+                        </ol>
+                      );
+                    })}
+                  </div>
+                )}
               </div>
             </div>
           </div>
